436d74b5ea250ac28cd49e548f155ef447321415
Kendric Terry <terrykendric@gmail.com>
2018-10-13 15:42:31 -0400

    Different Variables

--- data.js
+++ data.js
@@ -1,5 +1,47 @@
 // Example
-let message = "eskeetit";
+
+let message = "my name is kendric";
+let rubiks = "i like food"
+let computer = "and anime"
+
 console.log(message);
+console.log(rubiks);
+console.log(computer);
+
+
+console.log(typeof message);
+console.log(typeof rubiks);
+console.log(typeof computer);
+
+
+
+
+let kendric = 5;
+let me = 62;
+let terry = 9;
+console.log(kendric);
+console.log(me);
+console.log(terry);
+console.log(typeof kendric);
+console.log(typeof me);
+console.log(typeof terry);
+
+
+
+let iLikeAnime = true;
+let tomatoesAreGood = false;
+ console.log(iLikeAnime);
+ console.log(tomatoesAreGood);
+
+console.log(typeof iLikeAnime);
+console.log(typeof tomatoesAreGood);
+
+let zaire;
+console.log(zaire);
+
+kendric + me;
+console.log(kendric + me);
+
+
 
 // Type your code below!
