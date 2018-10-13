512f3f52c6fa61f879bafa75cb75b33e97cc262f
CodeNext <codenext@Admins-MBP-10.lan>
2018-10-13 15:44:34 -0400

    Schoolyard

--- data.js
+++ data.js
@@ -2,4 +2,23 @@
 let message = "Tianna";
 console.log(message);
 
-// Type your code below!
+//type your code below!
+let name = "Bella";
+console.log(name);
+
+let place = "schoolyard";
+console.log(place);
+
+let name = "Alex":
+console.log(name);
+
+let alexDoesSports = true;
+console.log(alexDoesSports);
+let alexAndBellaFriends = false;
+
+let theirAge = 14;
+console.log(theirAge);
+let yearsOfSchoolYard = 50;
+console.log(yearsOfSchoolYard);
+let yearsTheyKnowEachOther = 3;
+console.log(yearsTheyKnowEachOther);
