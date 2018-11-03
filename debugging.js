99858ddcdd843bcb40f16fb5a0733db84c7542a4
Tianna Wilson <tiannawilson8@gmail.com>
2018-11-03 15:21:51 -0400

    Bugs!

--- debugging.js
+++ debugging.js
@@ -1,30 +1,31 @@
 // Remove block comments when ready to debug a program.
-
+const READLINE = require("readline-sync");
 // Program #1: "Careless"
-/*
-let x == 3;
-if(x => 3 {
-  X+++;
-}} Else {
-  X =+ 2;
 
-console.log = X;
-*/
+let x = 3;
+if(x => 3) {
+  x++;
+} else {
+  x =+ 2;
+}
+console.log(x);
+
 
 // Program #2: "Bad logic and math"
-/*
+
 let money = 28;
-if(money > 50 && money <= 20) {
+if(money <= 50 && money >= 20) {
   money /= 2;
 } else {
-  money /= 0;
+  money = 0;
 }
-*/
+ console.log(money)
+
 
 // Program #3: "And what else?"
-/*
+
 let num1 = READLINE.question("Enter num1: ");
-let num2 = READLINE.question("Enter num2: ");
+let num2 = READLINE.question("pnter num2: ");
 let num3 = READLINE.question("Enter num3: ");
 if(num1 > num2) {
   console.log(`${num1} is the largest value.`);
@@ -35,4 +36,5 @@ else if(num2 > num3) {
 else {
   console.log(`${num3} is the largest value.`);
 }
-*/
+console.log(num1)
+
